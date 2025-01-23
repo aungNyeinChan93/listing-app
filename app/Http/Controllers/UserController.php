ad4e0faef8250608865ab429fbf9c0ec4110023b
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-01-23 17:18:55 +0630

    serach bar add

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,15 +10,26 @@ class UserController extends Controller
 {
     //index
 
-    public function index(){
-        return Inertia::render('Users/Index',[
-            'users'=>User::query()->paginate(10),
+    public function index(Request $request)
+    {
+
+        $users = User::query()
+            ->when($request->search, function ($query) use ($request) {
+                $query->whereAny(['name', 'email'], 'like', "%{$request->search}%");
+            })
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+
+
+        return Inertia::render('Users/Index', [
+            'users' => $users,
         ]);
     }
 
-    public function show(User $user){
-        return Inertia::render('Users/Show',[
-            'user'=>$user,
+    public function show(User $user)
+    {
+        return Inertia::render('Users/Show', [
+            'user' => $user,
         ]);
     }
 }
--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -1,7 +1,12 @@
 <template>
+
     <Head title="Users" />
     <MyContainer>
-    <UserTable :users="users" routeName='users.show' />
+        <div class="grid grid-cols-12">
+            <SearchInput class="col-span-3 col-start-10" v-model="search" type="text" label="search" placeholder="serach ... " :action="action" />
+        </div>
+        <!-- <div v-if="users"> {{ users }}</div> -->
+        <UserTable :users="users" routeName='users.show' />
         <div class="pagination text-end mt-3">
             <Pagination :links="users.links" />
         </div>
@@ -12,6 +17,10 @@
 import MyContainer from "../Components/MyContainer.vue";
 import UserTable from "../Components/UserTable.vue";
 import Pagination from "../Components/Pagination.vue";
+import SearchInput from "../Components/SearchInput.vue";
+import { ref, watch } from "vue";
+import { router } from "@inertiajs/vue3";
+import { throttle } from 'lodash';
 
 const props = defineProps({
     users: {
@@ -20,4 +29,15 @@ const props = defineProps({
 
     }
 });
+
+const search = ref('');
+
+const action = () => {
+    router.get('/users', { search: search.value }, { preserveState: true });
+};
+
+watch(search, throttle((value) => {
+    router.get('/users', { search: value }, { preserveState: true });
+}, 500));
+
 </script>
